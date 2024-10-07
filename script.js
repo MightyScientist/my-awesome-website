b213fb315250f40e256b188c143f3ca65a24eab6
MightyScientist <mightyscientist3214@gmail.com>
2024-10-07 13:14:59 -0400

    just new stuff

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function showMessage() {
     alert('You clicked the button! Congrats!');
+    document.querySelector("h1").innerText = "Thank you for visiting!";
+}
+
+function toggleInterests() {
+    const interestsList = document.getElementById('interests');
+    interestsList.style.display = interestsList.style.display === 'none' ? 'block' : 'none';
 }
